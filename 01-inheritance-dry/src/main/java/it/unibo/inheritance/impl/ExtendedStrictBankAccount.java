6405e1a1eabece65e4748986737ffdca863eadd2
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-11-11 09:55:29 +0100

    added methods from StrictBankAccount that are not redoundant and made them work

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -1,9 +1,35 @@
 package it.unibo.inheritance.impl;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount{
+
     private static final double TRANSACTION_FEE = 0.1;
 
     public ExtendedStrictBankAccount(final int id, final double balance) {
         super(id, balance);
+    }
+
+    private void transactionOp(final int id, final double amount) {
+        if (checkUser(id)) {
+            super.setBalance(super.getBalance() + amount);
+            this.incrementTransactions();
+        }
+    }
+
+    public void withdraw(final int id, final double amount) {
+        if (isWithdrawAllowed(amount)) {
+            this.transactionOp(id, -amount);
+        }
+    }
+
+    public void chargeManagementFees(final int id) {
+        final double feeAmount = MANAGEMENT_FEE + super.getTransactionsCount() * TRANSACTION_FEE;
+        if (checkUser(id) && isWithdrawAllowed(feeAmount)) {
+            super.setBalance(super.getBalance() - feeAmount);
+            resetTransactions();
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount) {
+        return getBalance() >= amount;
     }
 }
