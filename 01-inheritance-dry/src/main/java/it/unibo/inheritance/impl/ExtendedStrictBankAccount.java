81d1340ee8b4f8997bb1e433a341bfef13fa84d0
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-11-11 09:38:04 +0100

    added constructor to ExtendedStrictBankAccount

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -1,4 +1,9 @@
 package it.unibo.inheritance.impl;
 
-public class ExtendedStrictBankAccount {
+public class ExtendedStrictBankAccount extends SimpleBankAccount{
+    private static final double TRANSACTION_FEE = 0.1;
+
+    public ExtendedStrictBankAccount(final int id, final double balance) {
+        super(id, balance);
+    }
 }
